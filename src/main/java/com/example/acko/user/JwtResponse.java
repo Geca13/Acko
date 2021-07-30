44d9518bde0e3092d2eef045ca7ba32a12f31e87
Geca13 <karafiloski84@gmail.com>
2021-07-30 14:58:00 +0200

    C:/Program Files/Git/

--- src/main/java/com/example/acko/user/JwtResponse.java
+++ src/main/java/com/example/acko/user/JwtResponse.java
@@ -4,39 +4,17 @@ import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JwtResponse {
     private boolean success;
     private String token;
+    
 
-    public JwtResponse(boolean success, String token) {
-        this.success = success;
-        this.token = token;
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    @Override
-    public String toString() {
-        return "JwtResponse{" +
-                "success=" + success +
-                ", token='" + token + '\'' +
-                '}';
-    }
+   
 }
